16fa255816675a548207a0268f5594843158e88e
TianjiZ <128509320+TianjiZ@users.noreply.github.com>
2023-12-08 01:08:48 +0000

    Add files via upload

--- primes.py
+++ primes.py
@@ -1,6 +1,26 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
-def primes(number_of_primes):
-    list = []
-    return list
+def check_prime(number):
+    """Determine if a number is a prime number."""
+    if number < 2:
+        return False
+    for divisor in range(2, int(number**0.5) + 1):
+        if number % divisor == 0:
+            return False
+    return True
+
+def primes(count):
+    """Generate a list of prime numbers up to the specified count."""
+    if count <= 0:
+        raise ValueError("The count must be a positive integer")
+
+    prime_numbers = []
+    current_number = 2
+    while len(prime_numbers) < count:
+        if check_prime(current_number):
+            prime_numbers.append(current_number)
+        current_number += 1
+
+    return prime_numbers
+
